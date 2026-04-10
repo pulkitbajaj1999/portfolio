fc7c54cc2d1598d2d1ffe0cd42392110ac23519a
pulkit <pulkit171112236@gmail.com>
2026-04-11 00:55:26 +0530

    base path in config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000, // Matches the old CRA port
-    open: true, // Opens browser on start
-  },
-});
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all envs regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      open: false
+    },
+    // Use an env variable from Railway, or default to root '/'
+    base: env.BASE_PATH || '/',
+  }
+})
